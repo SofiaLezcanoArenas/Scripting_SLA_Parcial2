48db30a51a9f4211c01d906df218f3ac08e78e1d
SofiaLezcanoArenas <sofia.lezcano.arenas@gmail.com>
2025-09-24 06:20:42 -0500

    se crea el proyecto de visual

---
+++ Parcial2OOPyTDD/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace Parcial2OOPyTDD
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
